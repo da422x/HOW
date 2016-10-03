3f7ecc3106ae9b983e19c54ef87b705bd7cbf3ae
Thaddeus Madison Jr <thaddeus.madison@gmail.com>
2016-10-03 17:22:18 -0500

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mainAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the mainAppApp
+ */
+angular.module('mainAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
