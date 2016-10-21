d69d1050ec296493e434a2907a0ad96db7e08f76
Rusu, Derek (dr513h) <dr513h@us.att.com>
2016-10-21 13:47:51 -0500

    Added page requesting interceptor, removed rudundant files: manageusers, logging interceptor, etc. Added some notes.

---
+++ app/scripts/controllers/user_permissions.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mainAppApp.controller:UserPermissionsCtrl
+ * @description
+ * # UserPermissionsCtrl
+ * Controller of the mainAppApp
+ */
+angular.module('mainAppApp')
+  .controller('UserPermissionsCtrl', ['$rootScope', 'commonServices', function ($rootScope, commonServices) {
+    
+  }]);
--- test/spec/controllers/user_permissions.js
+++ test/spec/controllers/user_permissions.js
@@ -1,23 +1,23 @@
 'use strict';
 
-describe('Controller: ManageusersCtrl', function () {
+describe('Controller: UserPermissionsCtrl', function () {
 
   // load the controller's module
   beforeEach(module('mainAppApp'));
 
-  var ManageusersCtrl,
+  var UserPermissionsCtrl,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    ManageusersCtrl = $controller('ManageusersCtrl', {
+    UserPermissionsCtrl = $controller('UserPermissionsCtrl', {
       $scope: scope
       // place here mocked dependencies
     });
   }));
 
   it('should attach a list of awesomeThings to the scope', function () {
-    expect(ManageusersCtrl.awesomeThings.length).toBe(3);
+    expect(UserPermissionsCtrl.awesomeThings.length).toBe(3);
   });
 });
