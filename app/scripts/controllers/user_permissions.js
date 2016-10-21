28d0c0aac758db11c3508fc59092d4d2b97b35ea
Rusu, Derek (dr513h) <dr513h@us.att.com>
2016-10-21 15:48:25 -0500

    changes to main page and permissions view

--- app/scripts/controllers/user_permissions.js
+++ app/scripts/controllers/user_permissions.js
@@ -9,5 +9,18 @@
  */
 angular.module('mainAppApp')
   .controller('UserPermissionsCtrl', ['$rootScope', 'commonServices', function ($rootScope, commonServices) {
-    
+    var user = this;
+
+    user.roles = [];
+
+    user.getAllUsers = function() {
+    	var roleData = commonServices.getData('/userRoles/')
+    		.then(function(data) {
+    			user.roles = data;
+    			console.log(user.roles);
+    		});
+    }
+
+    user.getAllUsers();
+
   }]);
