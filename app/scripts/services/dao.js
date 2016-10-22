df648acc95b55ef792a7daf3f544596198c7056c
Thaddeus Madison Jr <thaddeus.madison@gmail.com>
2016-10-22 10:57:55 -0500

    updated dao objects: chapter has many reports and added location metadata

--- app/scripts/services/dao.js
+++ app/scripts/services/dao.js
@@ -8,40 +8,40 @@
  * Service in the mainAppApp.
  */
 angular.module('mainAppApp')
-    .service('DAO', function() {
+    .service('DAO', function () {
         // AngularJS will instantiate a singleton by calling "new" on this function
 
         var userData = function userData() {
-			this.address= null;
-            this.branch= null;
-            this.email= null;
-            this.emergency= {
+            this.address = null;
+            this.branch = null;
+            this.email = null;
+            this.emergency = {
                 name: null,
                 phone: null
-            };
-            this.gender= null;
-            this.name= {
+            }; //emergency should be upgraded to a list of emergency contacts
+            this.gender = null;
+            this.name = {
                 first: null,
                 last: null
             };
-            this.phone= null;
-            this.years= null
+            this.phone = null;
+            this.years = null
             if (arguments.length == 1) {
                 angular.extend(this, arguments[0]);
             }
         }
 
         var report = function report() {
-            this.prototype = {
-                name: null,
-                url: null,
-                data: null,
-                regionId: null,
-                chapterId: null,
-                creatorId: null,
-                lastModifiedId: null,
-                typeOfReport: null
-            }
+
+            this.name = null;
+            this.url = null;
+            this.data = null;
+            this.regionId = null;
+            this.chapterId = null;
+            this.creatorId = null;
+            this.lastModifiedId = null;
+            this.typeOfReport = null;
+
 
             if (arguments.length == 1) {
                 angular.extend(this.prototype, arguments[0]);
@@ -50,20 +50,41 @@ angular.module('mainAppApp')
         }
 
         var chapter = function chapter() {
-            this.prototype = {
-                users: null,
-                location: {},
-                staff: {},
-                events: {
-                    description: null,
-                    name: null,
-                    data: null,
-                    images: null,
-                    participants: null,
-                    volunteers: null,
-                    LTMs: null
+
+            //for events, we should implement a set of hardcoded color css classes so we 
+            //can visually show how soon the event is.
+            //images is an array of objects with a name and url for the location of the image
+            this.users = null;
+            this.location = {
+                address: null,
+                address2: null,
+                city: null,
+                state: null,
+                zip: null
+            };
+            this.staff = {};
+            this.events = {
+                title: null,
+                allDay: null,
+                start: null,
+                end: null,
+                description: null,
+                name: null,
+                data: null,
+                images: {},
+                participants: null,
+                volunteers: null,
+                LTMs: null,
+                location: {
+                    address: null,
+                    address2: null,
+                    city: null,
+                    state: null,
+                    zip: null
                 }
-            }
+            };
+            this.reports = {};//list of reports that are under a chapter. report types may vary
+
 
             if (arguments.length == 1) {
                 angular.extend(this.prototype, arguments[0]);
