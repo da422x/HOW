300622c40428ce6d394c08302715f7d5359519bd
Thaddeus Madison Jr <thaddeus.madison@gmail.com>
2016-11-14 22:36:54 -0600

    refactor of new expense and using the expense service

--- app/expense/scripts/services/expenseservice.js
+++ app/expense/scripts/services/expenseservice.js
@@ -12,7 +12,44 @@ angular.module('mainAppApp')
   .service('expenseServices', function(filterFilter) {
   //['$rootScope', '$firebaseAuth', 'DAO' , function ($rootScope, $firebaseAuth, DAO) {
 
-  $scope.expense = [];
+
+  this.expense = {
+      BillId: "",
+      Chapter: "MARYLAND CHAPTER",
+      eventdate: "",
+      SubmitDate: "",
+      Description: "",
+      PaymentStatus: "",
+      Amount: 0,
+      ImageURL:[],
+      Line: [{
+          "ID": "1",
+          "Description": "Mileage Rate - Travel @.25/mile",
+          "Quantity":null,// this.exp.miles,
+          "Amount": 0//(this.exp.miles * .25)
+      }, {
+          "ID": "2",
+          "Description": "Trailer Mileage Rate @.40/mile",
+          "Quantity": null,//this.exp.trailermiles,
+          "Amount": 0//(this.exp.trailermiles * .4)
+      }, {
+          "ID": "3",
+          "Description": "",
+          "Quantity": "1",
+          "Amount": null
+      }, {
+          "ID": "4",
+          "Description": "",
+          "Quantity": "1",
+          "Amount": null
+      }]
+
+  }
+
+  this.addNewImage = function(obj){
+    this.expense.ImageURL.push(obj);
+  }
+
 var ref = firebase.database().ref('/expense').on('value', function(snapshot) {   
       $scope.expense =snapshot.val(); 
        
@@ -22,9 +59,15 @@ var ref = firebase.database().ref('/expense').on('value', function(snapshot) {
     return expense;
   };
 
+this.addNewList = function(line){
+  expense.Line.push()
+
+}
+
   this.getExpenseAt = function(_billid) {
     this.getExpense();
     return filterFilter(expense, {
       BillId: _billid
     })[0];
   };
+})
