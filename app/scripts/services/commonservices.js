137f337b4e734d7e48e7bcfb2cebeb9947c9ba92
ARROYO <da422x@att.com>
2016-11-16 00:26:09 -0600

    added events page

--- app/scripts/services/commonservices.js
+++ app/scripts/services/commonservices.js
@@ -177,6 +177,31 @@ angular.module('mainAppApp')
 			});
 	};
 
+	this.getPublicEvents = function() {
+		var email = 'public@test.com';
+		var password = '123456';
+
+		firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+		  var errorCode = error.code;
+		  var errorMessage = error.message;
+		  console.log('ERROR: ' + error.code + ': ' + error.message);
+		});
+
+		return firebase.database().ref('events')
+			.once('value')
+			.then(function(snapshot) {
+				console.log('Data received');
+				return snapshot.val();
+			})
+			.catch(function(error){
+				console.log('uh oh');
+
+				var errorCode = error.code;
+	  			var errorMessage = error.message;
+				console.log('ERROR: ' + error.code + ': ' + error.message);
+			});
+	};
+
 	/******************************************************
 	*                  C.R.U.D. - end                     *
 	*******************************************************/
