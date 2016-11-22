5e6a9d6145cdcc7b556bf7d1399e68886384c9e7
Rusu, Derek (dr513h) <dr513h@us.att.com>
2016-11-22 11:03:47 -0600

    add lodash back in

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -60,6 +60,7 @@ module.exports = function(config) {
       'bower_components/angularfire/dist/angularfire.js',
       'bower_components/mockfirebase/browser/mockfirebase.js',
       'bower_components/ngmap/build/scripts/ng-map.js',
+      'bower_components/lodash/lodash.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
