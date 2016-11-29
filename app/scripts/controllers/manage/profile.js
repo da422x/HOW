97fb7ca8c0547247a1c2677f4cf26ae0cb28bfd1
Rusu, Derek (dr513h) <dr513h@us.att.com>
2016-11-29 13:07:24 -0600

    Setting up page for user profile

---
+++ app/scripts/controllers/manage/profile.js
@@ -0,0 +1,15 @@
+/*jslint browser: true, devel: true, bitwise: true, eqeq: true, plusplus: true, vars: true, indent: 4*/
+/*global angular, $, console, swal*/
+
+/**
+ * @ngdoc function
+ * @name ohanaApp.controller:ProfileCtrl
+ * @description
+ * # ProfileCtrl
+ * 
+ */
+angular.module('ohanaApp')
+	.controller('ProfileCtrl', function () {
+		'use strict';
+		// empty controller
+	});
