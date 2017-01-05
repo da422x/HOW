80736954a995681326276db6936e544c03811de5
Rusu, Derek (dr513h) <dr513h@us.att.com>
2017-01-05 14:10:09 -0600

    more updates

--- app/scripts/controllers/manage/chapterchat.js
+++ app/scripts/controllers/manage/chapterchat.js
@@ -15,15 +15,17 @@ angular.module('ohanaApp')
         $scope.chatLog = [];
 
         $scope.$watch('chatLog', function() {
-        	 $('.panel-body').animate({scrollTop: $('.panel-body').get(0).scrollHeight});
+            $('.panel-body').animate({
+                scrollTop: $('.panel-body').get(0).scrollHeight
+            });
         });
 
         $scope.init = function() {
             var usersChapter = $rootScope.userChapter;
             var getLog = firebase.database().ref('/chat/chapters/' + usersChapter)
-	            .on('value', function(snapshot) {
-	                $scope.chatLog = _.sortBy(snapshot.val(), ['time']);
-	            });
+                .on('value', function(snapshot) {
+                    $scope.chatLog = _.sortBy(snapshot.val(), ['time']);
+                });
         };
 
         $scope.sendMessage = function() {
