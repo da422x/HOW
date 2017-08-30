9d76c5f50e98500d47e1b76ee41edbded6aaa362
Thaddeus Madison Jr <thaddeus.madison@gmail.com>
2017-08-29 23:30:26 -0500

    merging

--- dist/assets/compatibility.js
+++ dist/assets/compatibility.js
@@ -294,9 +294,9 @@
           // and if not first of each 4 characters,
           // convert the first 8 bits to one ascii character
           bc++ % 4
-        ) ?
-        (output += String.fromCharCode(255 & (bs >> ((-2 * bc) & 6)))) :
-        0
+        )
+          ? (output += String.fromCharCode(255 & (bs >> ((-2 * bc) & 6))))
+          : 0
       ) {
         // try to find character in table (0-63, not found => -1)
         buffer = digits.indexOf(buffer);
