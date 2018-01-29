205ecd7bfb49b209485d835b1983cb0fadc6d83b
Derek Rusu <derek.rusu@gmail.com>
2018-01-28 21:15:50 -0600

    WIP stil working on add participants, just adding my changes.

--- app/parts/addParticipantsToEvent.js
+++ app/parts/addParticipantsToEvent.js
@@ -21,12 +21,16 @@ angular
   ) {
     'use strict';
 
-    console.log(event);
-
     $scope.initialize = function() {
 
+      $scope.clearSearch();
       $scope.searchTypes = ['Name', 'Phone', 'Chapter', 'Email'];
-      $scope.selectedType = 'Name';
+      $scope.searchType = 'Name';
+      $scope.chapters = $rootScope.siteData.chapters;
+      $scope.searchChapter = $scope.chapters[0];
+      $scope.foundParticipant = [];
+      $scope.selectedParticipant = [];
+
     };
 
     $scope.addParticipantToCurrentEvent = function(eventKey, userKey) {
@@ -52,6 +56,72 @@ angular
 
         }
 
+    };
+
+    $scope.runSearch = function() {
+
+      //
+      var searchPromise;
+
+      console.log($scope.searchType);
+
+
+      switch($scope.searchType) {
+        case 'Name':
+          break;
+        case 'Email':
+          break;
+        case 'Phone':
+          break;
+        case 'Chapter':
+          if ($scope.searchChapter) {
+
+            searchPromise = commonServices.queryChapterkey($scope.searchChapter.key);
+
+            $q.all([searchPromise]).then(function(data) {
+              if (data[0]) {
+                _.each(data[0], function(user, key) {
+                  user.nameText = user.name.first + ' ' + user.name.last;
+                  user.key = key;
+                  $scope.foundParticipants.push(user);
+                });
+              }
+            });
+
+          }
+          break;
+        default:
+          break;
+
+      }
+
+    };
+
+    $scope.addFoundParticipant = function() {
+      if ($scope.foundParticipant[0]) {
+        var selectedAlready = false;
+        _.each($scope.selectedParticipants, function(selectedParts) {
+          if (selectedParts.key === $scope.foundParticipant[0].key) {
+            $scope.selectedAlready = true;
+            return;
+          }
+        });
+        if (selectedAlready) {
+          
+        } else {
+          $scope.selectedParticipants.push($scope.foundParticipant[0]);
+        }
+      }
+    };
+
+    $scope.clearSearch = function() {
+      $scope.searchFirst = false;
+      $scope.searchLast = false;
+      $scope.searchEmail = false;
+      $scope.searchPhone = false;
+      $scope.searchChapter = false;
+      $scope.foundParticipants = [];
+      $scope.selectedParticipants = [];
     };
     
     $scope.cancel = function() {
